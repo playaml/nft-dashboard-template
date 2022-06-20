327dc7ecbd5bb67b02c67ddeb58fef97faa049a2
Coin Computer <nospaceleftondevice@gmail.com>
2022-06-20 04:04:42 -0400

    opt out of Google floc

--- vendor/github.com/dabit3/lens-protocol-frontend/next.config.js
+++ vendor/github.com/dabit3/lens-protocol-frontend/next.config.js
@@ -7,6 +7,19 @@ const nextConfig = {
     assetPrefix: 'lens-protocol-frontend/',
     domains: ['ipfs.infura.io'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'interest-cohort=()',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
