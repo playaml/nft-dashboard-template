f3bc64195bf7ef435bb5511d7d12d0147c341b2d
Coin Computer <nospaceleftondevice@gmail.com>
2022-06-20 04:19:10 -0400

    add blank assetPrefix

--- vendor/github.com/dabit3/lens-protocol-frontend/next.config.js
+++ vendor/github.com/dabit3/lens-protocol-frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     loader: 'akamai',
     path: '',
-//    assetPrefix: 'lens-protocol-frontend/',
+    assetPrefix: '',
     domains: ['ipfs.infura.io'],
   },
   async headers() {
